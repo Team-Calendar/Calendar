fcddfc91825a129e1379bc36919fbb0b3c28ffb2
Alexander <adamqnov07@gmail.com>
2021-07-01 15:32:34 +0300

    add new classes

---
+++ Calendar/Calendar/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calendar
+{
+    class Message
+    {
+        public void PrintOption()
+        {
+
+        }
+    }
+}
---
+++ Calendar/Calendar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            UserMenu menu = new UserMenu(a);
+
+            menu.UserMneu();
+        }
+    }
+}
