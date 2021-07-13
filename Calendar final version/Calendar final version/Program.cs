ebe5d8436754d00ba1b4061cb0729e772c088eea
Dimitar <65706772+dimitar-13@users.noreply.github.com>
2021-07-13 15:11:03 +0300

    Update Program.cs

--- Calendar final version/Calendar final version/Program.cs
+++ Calendar final version/Calendar final version/Program.cs
@@ -6,17 +6,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Calendar_final_version
-{
+{ 
     class Program
-            //#daPipnemCoda
+
     {
 
-        public static void ValidateData(string NewName, DateTime StartTime, DateTime EndTime, string Place, string Comment)
+        private static void ValidateData(string NewName, DateTime StartTime, DateTime EndTime, string Place, string Comment)
         {
             if (string.IsNullOrEmpty(NewName) || string.IsNullOrEmpty(Place) || string.IsNullOrEmpty(Comment))
             {
                 Console.WriteLine("You entered a non-valid information!");
-                         Console.ReadLine();
+                Console.ReadLine();
                 Console.Clear();
                 CreateEvent();
             }
@@ -26,19 +26,39 @@ namespace Calendar_final_version
                 Console.ReadLine();
                 Console.Clear();
                 CreateEvent();
-            }   
+            }
         }
-
-        public static void CreateEvent()
+        private static void CreateEvent()
         {
             Console.WriteLine("Enter your new event name.");
             string NewName = Console.ReadLine();
             Console.WriteLine("Enter new Date!/mm.dd.yyyy");
-            DateTime Date = DateTime.Parse(Console.ReadLine());
+            string a = Console.ReadLine();
+            DateTime Date = DateTime.MinValue;
+            if (!DateTime.TryParse(a, out Date))
+            {
+                Console.WriteLine("Please enter valid date");
+                CreateEvent();
+                return;
+            }
             Console.WriteLine("Enter new StartTime!/hh:mm:ss");
-            DateTime StartTime = DateTime.Parse(Console.ReadLine());
+            string Starttime = Console.ReadLine();
+            DateTime StartTime = DateTime.MinValue;
+            if (!DateTime.TryParse(Starttime, out StartTime))
+            {
+                Console.WriteLine("Please enter valid date");
+                CreateEvent();
+                return;
+            }
             Console.WriteLine("Enter mew EndTime!/hh:mm:ss");
-            DateTime EndTime = DateTime.Parse(Console.ReadLine());
+            string Endtime = Console.ReadLine();
+            DateTime EndTime = DateTime.MinValue;
+            if (!DateTime.TryParse(Endtime, out EndTime))
+            {
+                Console.WriteLine("Please enter valid date");
+                CreateEvent();
+                return;
+            }
             Console.WriteLine("Enter new event Place.");
             string Place = Console.ReadLine();
             Console.WriteLine("Enter comment.");
@@ -47,7 +67,7 @@ namespace Calendar_final_version
             Events User = new Events(NewName, Date, StartTime, EndTime, Place, Comment);
             User.CreateEvent();
         }
-        public static void Choise()
+        private static void Choise()
         {
             Console.WriteLine("Please choose what to do (1 - Create an event | 2-Cancel | 3 - Edit | 4 - Daily schedule | 5-Find availability)");
             Events EventEditor = new Events();
@@ -78,16 +98,16 @@ namespace Calendar_final_version
         static void Main(string[] args)
         {
             Choise();
-            Console.WriteLine("Do you want to continue?");
+            Console.WriteLine("Do you want to return in the user menu?/Yes or No:");
             string answer = Console.ReadLine();
-            while (answer == "Yes"|| answer == "y"|| answer == "yes")
-            {            
+            while (answer == "Yes" || answer == "y" || answer == "yes")
+            {
                 if (answer == "Yes" || answer == "y" || answer == "yes")
                 {
                     Choise();
                     Console.WriteLine("Do you want to continue?");
                     string answer2 = Console.ReadLine();
-                    if (answer2 =="No"|| answer2 == "n"|| answer2 == "No")
+                    if (answer2 == "No" || answer2 == "n" || answer2 == "No")
                     {
                         break;
                     }
